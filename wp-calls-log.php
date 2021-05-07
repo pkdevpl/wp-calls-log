1a0fb683df68e995eab7cbd42f383ffa6ecca448
Piotr Kuculyma <67703644+pkdevpl@users.noreply.github.com>
2021-05-07 21:53:02 +0200

    Initial commit with plugin data

---
+++ wp-calls-log.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WP Calls Log
+Plugin URI: github.com/pkdevpl/wp-calls-log
+Description: Plugin registers incoming phone calls using Tasker App for Android and WP REST API. It can work with any app capable of detecting incoming phone calls and sending http requests.
+Author: Piotr Kucułyma
+Author URI: github.com/pkdevpl
+License: LGPL
+*/
+
